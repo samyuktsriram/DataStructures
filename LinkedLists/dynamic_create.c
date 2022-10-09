5f2633ee4c0e1f67b72b03ce5ec5b44dab8ee4c0
Samyukt Sriram <sriramsamyukt@gmail.com>
2022-10-09 18:40:06 +0530

    adding reversal recursive algo

--- LinkedLists/dynamic_create.c
+++ LinkedLists/dynamic_create.c
@@ -64,7 +64,7 @@ void loop_tester(Node* head){
     Node* rabbit = head;
     Node* tortoise = head;
     while(1){
-        printf("%d\n", tortoise->data);
+        //printf("%d\n", tortoise->data);
         if (rabbit->next == NULL){printf("No Loops here \n"); break;} else if (rabbit->next->next == NULL){printf("No Loops here\n"); break;}
         rabbit = rabbit->next->next;
         tortoise = tortoise->next;
@@ -73,13 +73,25 @@ void loop_tester(Node* head){
 
 }
 
-int main(void){
+Node* rec_reverse_linkedlist(Node* head){
 
-
-    //Hardcoding a start head node
-    Node* head = malloc(sizeof(Node));
-    head->data = 1;
+    if((!head) || !(head->next)){return head;}
+    Node* rest = rec_reverse_linkedlist(head->next);
+    head->next->next = head;
     head->next = NULL;
+    return rest;
+}
+
+Node* create_linkedlist(void){
+
+    Node* head = malloc(sizeof(Node));
+    int val_head;
+    printf("Enter the head element, enter -ve number to end\n");
+    scanf("%d",&val_head);
+    if(val_head<0){return head;}
+    head->data = val_head;
+    head-> next = NULL;
+
     Node* temp;
     temp = head;
 
@@ -98,6 +110,41 @@ int main(void){
         
     }
 
+    return head;
+
+}
+
+Node* skip_chunk(int k, Node* temp){
+    while(k>0){
+        temp = temp->next;
+        if(!(temp->next)){return NULL;}
+        k = k-1;
+    }
+    return temp;
+}
+
+Node* chunk_reverse(int chunk, Node* head){
+    Node* current = head;
+    Node* prev = NULL;
+    Node* temp = NULL;
+
+    while(current){
+        temp = skip_chunk(chunk,current);
+        current-> next = prev;
+        prev = current;
+        current = temp;
+
+    }
+    return prev;
+}
+
+int main(void){
+
+
+    //Creating the Head
+    Node* head = create_linkedlist();
+
+    
     print_linkedlist(head);
     print_half_tr(head);
 
@@ -107,12 +154,17 @@ int main(void){
 
     //head->next->next->next->next->next->next = head->next;
 
-    // Detecting a loop function -> works if there is a loop. Segment fault if there isn't, figure it out
+    // Detecting a loop function -> works if there is a loop.
 
     Node* tail = NULL;
     tail = reverse_linkedlist(head);
-
     print_linkedlist(tail);
+    Node* rec_tail = rec_reverse_linkedlist(tail);
+    print_linkedlist(rec_tail);
+    loop_tester(rec_tail);
+
+    Node* chunked = chunk_reverse(4,rec_tail);
+    print_linkedlist(chunked);
 
 
     return 0;
