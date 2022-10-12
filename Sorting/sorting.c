0d32d6e52a3ef4d39e12bd2f020474078a684491
Samyukt Sriram <sriramsamyukt@gmail.com>
2022-10-12 22:37:02 +0530

    selection sort

--- Sorting/sorting.c
+++ Sorting/sorting.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int* create_array(int length){
+int* create_array_input(int length){
 
     int* ptr = malloc(length * sizeof(int));
     for(int i=0; i<length; i++){
@@ -9,6 +10,40 @@ int* create_array(int length){
         scanf("%d", &ptr[i]);
     }
     return ptr;
+}
+
+int* create_array_rand(int length){
+
+    int* ptr = malloc(length * sizeof(int));
+    for(int i=0; i<length; i++){
+        ptr[i] = rand()%1000;
+    }
+    return ptr;
+}
+
+void selection_sort(int length, int* array){
+
+//Doesn't work lmao
+
+    //INV: 0-k is sorted, 0<=k<length-1
+    for(int k = 0; k < length - 1; k++){
+
+        //int smallest = array[k];
+
+        int smallest_index = k;
+        //INV: array[j] is the smallest element in array[k..length]
+        for(int j = k+1; j<length;j++){
+
+            //If condition to find index of smallest
+            if(array[j] < array[smallest_index]){
+                //swap
+                smallest_index = j;
+            }
+        }
+        int temp = array[k];
+        array[k] = array[smallest_index];
+        array[smallest_index] = temp;
+    }
 }
 
 void print_array(int length, int* array){
@@ -22,12 +57,18 @@ void print_array(int length, int* array){
 
 int main(){
 
+    srand(time(NULL));
+
     int len;
     printf("Enter the length of the array\n");
     scanf("%d", &len);
-    int* array = create_array(len);
+
+    int* array = create_array_input(len);
 
     print_array(len,array);
-    return 0;
+    selection_sort(len, array);
+    print_array(len, array);
 
+    free(array);
+    return 0;
 }
